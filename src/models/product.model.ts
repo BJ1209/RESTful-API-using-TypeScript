8443a1c7df240c9a66d3019fd052ace8d0a80716
Bikram Jeet Singh <me.bikram.dev@gmail.com>
2021-06-14 20:59:07 +0530

    Added image upload to products route

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -3,11 +3,13 @@ import { Schema, model, Document } from 'mongoose';
 interface ProductDocument extends Document {
   name: string;
   price: number;
+  productImage: string;
 }
 
 const ProductSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  productImage: { type: String, required: true },
 });
 
 const Product = model<ProductDocument>('Product', ProductSchema);
